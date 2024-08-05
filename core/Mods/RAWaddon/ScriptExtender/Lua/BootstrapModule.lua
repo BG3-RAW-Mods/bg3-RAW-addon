7afc9141f91fb48a9c816e330beb01769773200f
Arnaud Pourbaix <pourbaix.arnaud@gmail.com>
2024-08-05 08:11:10 +0200

    monk: deflect missile can only be thrown back if damage is 0

--- core/Mods/RAWaddon/ScriptExtender/Lua/BootstrapModule.lua
+++ core/Mods/RAWaddon/ScriptExtender/Lua/BootstrapModule.lua
@@ -1,3 +1,5 @@
+Ext.Require("Localizations.lua")
 Ext.Require("RAW/RAW_BootstrapShared.lua")
 
 RAW_ApplyModOptions()
+
---
+++ core/Mods/RAWaddon/ScriptExtender/Lua/Localizations.lua
@@ -0,0 +1,40 @@
+-- Interrupts
+Ext.Loca.UpdateTranslatedString("hce280aaag3a21g44f4g8076g9e5084933c3c", "Roll: [3].")
+Ext.Loca.UpdateTranslatedString("h5dce5131g0e79g417cg9df9g0f3a11ab5f8f", "Roll: [3] (Critical Hit).")
+Ext.Loca.UpdateTranslatedString("h24ce20fcgf0c7g496dgace7g22bd43f88dc8", "Roll: [3] (Critical Miss).")
+
+-- TODO: Need precisions to properly replace them
+-- Ext.Loca.UpdateTranslatedString("h6d0981cbgc0c5g40b3g8114g82c9b5eb121d", "[1] needs [2] to save. They rolled [3] (Critical Success).")
+-- Ext.Loca.UpdateTranslatedString("h45bccc1dgdb24g4748g9eeegbbb9909b6917", "[1] needs [2] to save. They rolled [3] (Critical Failure).")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_1", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_10", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_11", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_12", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_13", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_14", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_15", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_16", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_17", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_18", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_19", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_2", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_20", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_3", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_4", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_5", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_6", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_7", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_8", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h438d92f7g623dg4239gb196g1de87cbad902_9", "[1] needs [3] to pass. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("h6e74c814g547eg47b1gb4d2ga057c3e6dcb5", "[1] needs [3] to succeed. They rolled [2].")
+-- Ext.Loca.UpdateTranslatedString("hc596e63fg3ecag4ebdg8736gfe4d88dbb0a2", "[1] needs [3] to save. They rolled [2].")
+
+
+-- Partial Rest
+Ext.Loca.UpdateTranslatedString("h7e75fd8fgc164g436fgbbf7gc0b993f7fee8", "Story Rest")
+Ext.Loca.UpdateTranslatedString("hae826c89g4f7eg4a64g976fgf6cbebab7564", "You won't recover anything with a story rest. It is only usefull to progress through story because it is tied to resting.")
+
+-- Durable Feat
+Ext.Loca.UpdateTranslatedString("h71165ff5g7a9bg4779g8c39g4897776fe026", "You can use <LSTag Type='Spell' Tooltip='Shout_Raw_Short_Rest_Recovering_33'>Tend to your wounds</LSTag> when taking a <LSTag Tooltip='ShortRest'>Short Rest</LSTag>. Your <LSTag Tooltip='Constitution'>Constitution</LSTag> score is increased by 1, to a maximum of 20.")
+Ext.Loca.UpdateTranslatedString("hba1b3efag40e4g45e6gb775g4523353b4dbb", "Your <LSTag Tooltip='Constitution'>Constitution</LSTag> increases by 1, to a maximum of 20.<br><br>You can use <LSTag Type='Spell' Tooltip='Shout_Raw_Short_Rest_Recovering_33'>Tending to wounds</LSTag> when taking a <LSTag Tooltip='ShortRest'>Short Rest</LSTag>.")
+
